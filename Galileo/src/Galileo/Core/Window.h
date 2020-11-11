753e8d4383a864df72b52e8ac07c4133f2518918
amapoplar <amazingpoplar@gmail.com>
2020-11-11 17:39:05 +0800

    correct some bugs

--- Galileo/src/Galileo/Core/Window.h
+++ Galileo/src/Galileo/Core/Window.h
@@ -11,8 +11,8 @@ namespace Galileo {
 		uint32_t Height;
 
 		WindowProps(const std::string& title = "Galileo Engine",
-			uint32_t width = 1600,
-			uint32_t height = 900)
+			uint32_t width = 1680,
+			uint32_t height = 1240)
 			: Title(title), Width(width), Height(height)
 		{
 		}
